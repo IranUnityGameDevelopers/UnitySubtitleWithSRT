805b734d8c5d6e967df070aba848399aa54d855d
AminSojoudi <masojoudi@gmail.com>
2014-05-29 21:56:27 +0430

    * initial Commit

---
+++ Assets/Scripts/Models/SRTReader.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SRTReader : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/Views/TextView.cs
@@ -0,0 +1,24 @@
+﻿// wrriten bt Amin Sojoudi
+
+using UnityEngine;
+using System.Collections;
+
+public class TextView : MonoBehaviour 
+{
+	public static TextView Instance;
+
+	void Awake()
+	{
+		Instance = this;
+	}
+
+	// use this function to show text
+	public void setText(string _text)
+	{
+
+	}
+
+
+	public void 
+
+}
