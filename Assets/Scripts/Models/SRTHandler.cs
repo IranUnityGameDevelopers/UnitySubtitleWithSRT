906f47cc5318e636d15db3b81a61010eb4481468
AminSojoudi <masojoudi@gmail.com>
2014-05-30 20:38:02 +0430

    * added a function to show subtitles from point to point

--- Assets/Scripts/Models/SRTHandler.cs
+++ Assets/Scripts/Models/SRTHandler.cs
@@ -7,6 +7,9 @@ public class SRTHandler : MonoBehaviour
 	private SRTReader reader;
 
 
+	public int currentIndexOfSubtitle = -1;
+
+
 	void Start()
 	{
 		reader = gameObject.GetComponent<SRTReader>();
@@ -21,10 +24,35 @@ public class SRTHandler : MonoBehaviour
 	IEnumerator startSrt(List<SRT> array)
 	{
 		foreach (var item in array) {
+			currentIndexOfSubtitle = array.IndexOf(item);
 			TextView.Instance.setText(item.text);
 			yield return new WaitForSeconds(item.time);
 		}
+		currentIndexOfSubtitle = -1;
 		TextView.Instance.setText("Finished");
 	}
+
+
+	IEnumerator startSrtFromPointToPoint(List<SRT> array , int startPoint , int finalPoint)
+	{
+		if (finalPoint > array.Count) {
+			Debug.LogError("Final point is biger than array lenght");
+			return false;
+		}
+		if (startPoint > finalPoint) {
+			Debug.LogError("Final point is less than array startPoint");
+			return false;
+		}
+		for (int i = startPoint; i <= finalPoint; i++) {
+			currentIndexOfSubtitle = i;
+			TextView.Instance.setText(array[i].text);
+			yield return new WaitForSeconds(array[i].time);
+		}
+		currentIndexOfSubtitle = -1;
+		TextView.Instance.setText("Finished");
+	}
+	
+
+
 
 }
